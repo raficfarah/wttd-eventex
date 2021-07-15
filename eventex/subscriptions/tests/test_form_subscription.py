6c9842c04f1a16320a9a0af8060a14dcd9fb20b8
rafic1 <rafic@victorfontes.com>
2021-07-15 19:06:43 -0300

    Valida CPF

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -3,10 +3,42 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
         """Form must have 4 fields"""
+        form = SubscriptionForm()
         expected = ['name', 'cpf', 'email', 'phone']
-        self.assertSequenceEqual(expected, list(self.form.fields))
+        self.assertSequenceEqual(expected, list(form.fields))
+
+    def test_cpf_is_digit(self):
+        """CPF must only accept digits."""
+        form = self.make_validated(cpf='ABCD5678910')
+        #self.assertListEqual(['cpf'], list(form.errors))
+        #self.assertFormErrorMessage(form, 'cpf', 'CPF deve conter apenas números.')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+    
+    def test_cpf_has_11_digits(self):
+        """CPF must contain eleven digits"""
+        form = self.make_validated(cpf='1234')
+        #self.assertListEqual(['cpf'], list(form.errors))
+        #self.assertFormErrorMessage(form, 'cpf', 'CPF deve conter 11 dígitos.')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
+    def assertFormErrorMessage(self, form, field, msg):
+        errors = form.errors
+        errors_list= errors[field]
+        self.assertListEqual([msg], errors_list)
+
+    def make_validated(self, **kwargs):
+        valid = dict(name='Rafic Farah', cpf='12345678910',
+                    email='rafic@farah.com', phone='21-998366404')
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+
+        return form
