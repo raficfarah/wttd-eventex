16df8b0203a4241f5b4abfa7a1849d30c761a054
rafic1 <rafic@victorfontes.com>
2021-07-15 17:59:39 -0300

    Marca como pago

--- eventex/settings.py
+++ eventex/settings.py
@@ -111,13 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
