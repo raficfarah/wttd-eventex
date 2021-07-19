b658c0fa4b9165b8c23c8ea276b37db63d76383f
rafic1 <rafic@victorfontes.com>
2021-07-19 18:18:10 -0300

    Course como Abstract Base Class

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,5 +1,5 @@
 from django.db.models import manager
-from eventex.core.models import Contact, Speaker, Talk
+from eventex.core.models import Contact, Course, Speaker, Talk
 from django.contrib import admin
 from django.utils.html import format_html
 
@@ -36,3 +36,4 @@ class SpeakerModelAdmin(admin.ModelAdmin):
 
 admin.site.register(Speaker, SpeakerModelAdmin)
 admin.site.register(Talk)
+admin.site.register(Course)
