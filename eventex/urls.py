a3e8e1a39ba26a06b19b729c4ccfddda9fb5c423
Rafic Farah <raficfarah07@gmail.com>
2021-11-09 17:50:47 -0300

    refatora imports no URLCONF do projeto

--- eventex/urls.py
+++ eventex/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include
+from django.urls import path, include
 
 from eventex.core.views import home, speaker_detail, talk_list
 
