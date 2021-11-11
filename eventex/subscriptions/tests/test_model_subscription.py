01317014934ffe3ed6192d7409603b1b12d70c62
Rafic Farah <raficfarah07@gmail.com>
2021-11-11 19:48:50 -0300

    corrige teste para o campo paid

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -24,6 +24,6 @@ class SubscriptionModelTest(TestCase):
     def test_str(self):
         self.assertEqual('Rafic Farah', str(self.obj))
 
-    def test_page_default_to_False(self):
+    def test_paid_default_to_False(self):
         """By default, paid must be False."""
         self.assertEqual(False, self.obj.paid)
